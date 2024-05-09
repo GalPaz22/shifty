ae11eab70f836f479e955e1473633e59a6c31fe0
GalPaz <galpaz2210@gmail.com>
2024-05-09 10:39:36 +0300

    Merge remote changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,7 @@
 'use client';
+
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 import React, { useState } from "react";
 import CustomDatePicker from "./DatePicker";
 import DayCard from "./DayCard.js";
